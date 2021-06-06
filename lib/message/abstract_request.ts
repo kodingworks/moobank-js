a6cd84df564b2860a3da907aad018821c2f8fc25
Mohamad Rafi <rafi.randoni@gmail.com>
2021-06-06 23:55:24 +0700

    feat(request): Update request

--- lib/message/abstract_request.ts
+++ lib/message/abstract_request.ts
@@ -1,13 +1,62 @@
-import RequestInterface from './request_interface';
+import ClientInterface from '../client_interface'
+import Parameter from '../parameter'
+import RequestInterface from './request_interface'
+import ResponseInterface from './response_interface'
 
 export default abstract class AbstractRequest implements RequestInterface {
-  abstract getEndpoint(): string
-  abstract createResponse(): any
+  protected httpClient: ClientInterface
+  protected parameters: Parameter
 
+  /**
+   * set destination url
+   *
+   * @returns string
+   */
+  abstract getEndpoint(): string
+
+  /**
+   * manipulate and generate new response
+   *
+   * @param response ResponseInterface
+   */
+  abstract createResponse(response: any): ResponseInterface
+
+  /**
+   * init object
+   *
+   * @param httpClient
+   * @param parameters
+   */
+  initialize(httpClient: ClientInterface, parameters?: object | Parameter) {
+    this.httpClient = httpClient
+
+    if (!this.parameters) {
+      this.parameters = new Parameter()
+    }
+
+    if (parameters instanceof Parameter) {
+      this.parameters = parameters
+    } else {
+      Object.keys(parameters).forEach((key) => {
+        this.parameters.set(key, parameters[key])
+      })
+    }
+  }
+
+  /**
+   * set request method
+   *
+   * @returns string
+   */
   getMethod(): string {
     return 'GET'
   }
 
+  /**
+   * set request configuration
+   *
+   * @returns object
+   */
   getOption(): object {
     return {
       header: {
@@ -16,7 +65,28 @@ export default abstract class AbstractRequest implements RequestInterface {
     }
   }
 
+  /**
+   * data to be send
+   *
+   * @returns object
+   */
   getData(): object {
     return {}
+  }
+
+  /**
+   * send request
+   *
+   * @returns ResponseInterface
+   */
+  async send() {
+    const response = await this.httpClient.send(
+      this.getMethod(),
+      this.getEndpoint(),
+      this.getData(),
+      this.getOption()
+    )
+
+    return this.createResponse(response)
   }
 }
--- lib/message/request_interface.ts
+++ lib/message/request_interface.ts
@@ -1,7 +1,13 @@
+import ClientInterface from '../client_interface'
+import ResponseInterface from './response_interface'
+import Parameter from '../parameter'
+
 export default interface RequestInterface {
+  initialize(httpClient: ClientInterface, parameters: object | Parameter): void
   getEndpoint(): string
   getMethod(): string
   getData(): object
   getOption(): object
-  createResponse(): any
+  send(): any
+  createResponse(response: any): ResponseInterface
 }
